88f28c37a5ea3d875272478276bd99765bd53bc4
Bruno Tostes <bruno@savewhey.com.br>
2021-07-23 16:53:40 -0300

    Added Rack support to server

---
+++ README.md
@@ -0,0 +1,7 @@
+Pure Ruby Custom HTTP server.
+
+- setup to create a TCP server.
+- listen for connections.
+- start new threads.
+- process the connections.
+- serves up files the user asked for.
--- server.rb
+++ server.rb
@@ -1,28 +1,50 @@
 require 'socket'
 require 'active_support/all'
+require 'rack'
+require 'rack/lobster'
+require 'pry'
 require_relative 'request'
 require_relative 'response'
 
+APP = Rack::Lobster.new
 port = ENV.fetch('PORT', 2000).to_i
 server = TCPServer.new 2000
 
 puts "Listening on port #{port}.."
 
-def render(path)
-  full_path = File.join(__dir__, 'views', path)
-  if File.exists?(full_path)
-    Response.new(code: 200, body: File.binread(full_path))
-  else
-    Response.new(code: 404)
-  end
+def render(file:)
+  body = File.binread(file)
+  Response.new(
+    code: 200,
+    body: body,
+    headers: {
+      'Content-Type': 'text/html',
+      'Content-Length': body.length
+    }
+  )
+end
+
+def template_exists?(path)
+  File.exist?(path)
 end
 
 def route(request)
-  if request.path == '/'
-    render 'index.html'
+  path = request.path == '/' ? 'index.html' : request.path
+  full_path = File.join(__dir__, 'views', path)
+  # rack application will render every route that does not match a file
+  if template_exists?(full_path)
+    render file: full_path
   else
-    render request.path
+    status, headers, body = APP.call({
+                                       'REQUEST_METHOD' => request.method,
+                                       'PATH_INFO' => request.path,
+                                       'QUERY_STRING' => request.query
+                                     })
+    Response.new(code: status, body: body.join, headers: headers)
   end
+rescue => e
+  puts e.full_message
+  Response.new(code: 500)
 end
 
 loop do
